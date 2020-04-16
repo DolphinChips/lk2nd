6a786aee91ab4256873d00d8925760215b72fd1f
Dolphin von Chips <DoIphinChips@protonmail.com>
2020-04-16 14:33:40 +0500

    Add initial support for MSM8909

---
+++ project/msm8909-secondary.mk
@@ -0,0 +1,27 @@
+# top level project rules for the msm8909 project
+#
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+OUTBOOTIMG := $(BUILDDIR)/lk2nd.img
+
+# Enable fastboot display menu
+ENABLE_FBCON_DISPLAY_MSG := 1
+
+include $(LOCAL_DIR)/msm8909.mk
+
+# Use maximum verbosity
+DEBUG := 2
+DEFINES += LK_LOG_BUF_SIZE=16384
+
+# Avoid writing device info
+DEFINES += SAFE_MODE=1
+# Display as unlocked by default
+DEFINES += DEFAULT_UNLOCK=1
+
+# Use continuous splash from primary bootloader for display
+DISPLAY_USE_CONTINUOUS_SPLASH := 0
+
+APPSBOOTHEADER: $(OUTBOOTIMG)
+TARGET_USES_APPENDED_DTBS := 1
+ANDROID_BOOT_BASE := 0x80000000
+ANDROID_BOOT_CMDLINE := lk2nd
